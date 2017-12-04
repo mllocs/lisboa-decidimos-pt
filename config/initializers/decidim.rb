8aebf1de2cc4a27e33054e70dc3f24c09e2d5a66
Maxim Colls <collsmaxim@gmail.com>
2017-12-04 11:59:34 +0000

    Added pt locale

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -6,8 +6,8 @@ Decidim.configure do |config|
   config.authorization_handlers = ["ExampleAuthorizationHandler"]
 
   # Change these lines to set your preferred locales
-  config.default_locale = :en
-  config.available_locales = [:en, :ca, :es]
+  config.default_locale = :pt
+  config.available_locales = [:en, :ca, :es, :pt]
 
   # Geocoder configuration
   # config.geocoder = {
